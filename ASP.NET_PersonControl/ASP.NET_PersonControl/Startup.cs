39e6903384172bf0260b92371b4f5c00c0400774
Artem Syritsa <80500451040j@gmail.com>
2018-06-21 14:06:08 +0300

    Empty project

---
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_PersonControl.Startup))]
+namespace ASP.NET_PersonControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
