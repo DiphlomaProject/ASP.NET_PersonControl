d86b8392eb473cc27431c095e22c976a1c6e5bc0
Riskenzi <riskenzi@icloud.com>
2018-07-06 05:42:19 +0300

    notification fix foto style
    a

--- ASP.NET_PersonControl/ASP.NET_PersonControl/Controllers/NotificationController.cs
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/Controllers/NotificationController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ASP.NET_PersonControl.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +14,45 @@ namespace ASP.NET_PersonControl.Controllers
     [Authorize]
     public class NotificationController : Controller
     {
+        public ApplicationDbContext _context { get; set; } // cennect to data base;
+
         // GET: Notification
         public ActionResult Index()
         {
-            return View();
+            _context = new ApplicationDbContext();
+            string id = User.Identity.GetUserId();
+            ApplicationUser employee = _context.Users.SingleOrDefault(emp => emp.Id == id);
+            //get user image
+            string storageAccountName = "aspnetpersoncontrol";
+            string keyOne = "GfiRnxHVXsaluga4L4R0zZOy4Ken4VnF3xM7I66OC263LJ9Sf2BOQgX41+/WpBlA8vMB5aP4wN/Uh00OF4MdXw==";
+            string nameOfStorage = "storage";
+            StorageCredentials storageCredentials = new StorageCredentials(storageAccountName, keyOne);
+            CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            //get users folder
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(nameOfStorage);
+            //get users folder
+            string userFolder = ((ApplicationUser)_context.Users.SingleOrDefault(u => u.Id == id)).Email;
+            CloudBlobDirectory cloudBlobDirectory = cloudBlobContainer.GetDirectoryReference(userFolder);
+            //add file to sub dir
+            CloudBlockBlob cloudBlockBlob = cloudBlobDirectory.GetBlockBlobReference("AccountImage.jpg");
+            try
+            {
+                cloudBlockBlob.FetchAttributes();
+                long fileByteLength = cloudBlockBlob.Properties.Length;
+                employee.img = new byte[fileByteLength];
+                for (int i = 0; i < fileByteLength; i++)
+                {
+                    employee.img[i] = 0x20;
+                }
+                cloudBlockBlob.DownloadToByteArray(employee.img, 0);
+            }
+            catch
+            {
+                employee.img = new byte[8];
+            }
+            return View(employee);
         }
     }
 }
--- ASP.NET_PersonControl/ASP.NET_PersonControl/Scripts/SignalR.js
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/Scripts/SignalR.js
@@ -6,5 +6,5 @@
     .fail(function () { alert("ItFail connection.hub.start()") });
 
 $.connection.hubSignalR.client.sendMessage = function (chatId, message) {
-    $("#messages").append(chatId + " : " + message + "<br/>");
+    $("#messages").append(" <tr><td> <img src=\"" + $("#img_src").val() + "\"  alt=\"Avatar\" style=\"width: 65px;height :65px;border-radius: 50%;\"/> </td> <td style=\"text-align: center;vertical-align: middle; \">" + chatId + "</td>  <td style=\"text-align: center;vertical-align: middle; \">" + message + "</td></tr>");
 }
