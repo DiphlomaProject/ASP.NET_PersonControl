934044f3a3ebbe61cb0c146e84e132b016e46a6b
Artem Syritsa <80500451040j@gmail.com>
2018-08-08 21:50:53 +0300

    Update logic groups

--- ASP.NET_PersonControl/ASP.NET_PersonControl/ViewModels/GroupsViewModel.cs
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/ViewModels/GroupsViewModel.cs
@@ -8,7 +8,11 @@ namespace ASP.NET_PersonControl.ViewModels
 {
     public class GroupsViewModel
     {
-        public IEnumerable<Groups> groups { get; set; }
         public ApplicationUser user { get; set; }
+        //public IEnumerable<UsersGroups> usersOfCurrentGroups { get; set; }
+        public IEnumerable<Groups> groups { get; set; }
+        //public IEnumerable<Projects> projectsOfCurrentGroups { get; set; }
+        //public IEnumerable<FilesGroups> filesOfGroups { get; set; }
+        //pulic IEnumerable<Files> files { get; set; }
     }
 }
