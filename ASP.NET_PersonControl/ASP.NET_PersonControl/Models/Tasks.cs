90d5502d4edbacf0373098b0ce20e3958001af49
Artem Syritsa <80500451040j@gmail.com>
2018-09-14 20:58:43 +0300

    Comit
    d

---
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/Models/Tasks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_PersonControl.Models
+{
+    public class TasksForUser
+    {
+        public int Id { get; set; }
+        public String toUserId { get; set; }
+        public String fromUserId { get; set; }
+        public String description { get; set; }
+        public DateTime dateTimeBegin { get; set; }
+        public DateTime dateTimeEnd { get; set; }
+        public Boolean isComplite { get; set; }
+    }
+
+    public class TasksForGroups
+    {
+        public int Id { get; set; }
+        public int toGroupId { get; set; }
+        public String fromUserId { get; set; }
+        public String description { get; set; }
+        public DateTime dateTimeBegin { get; set; }
+        public DateTime dateTimeEnd { get; set; }
+        public Boolean isComplite { get; set; }
+    }
+
+    public class TasksForProjects
+    {
+        public int Id { get; set; }
+        public int toProjectId { get; set; }
+        public String fromUserId { get; set; }
+        public String description { get; set; }
+        public DateTime dateTimeBegin { get; set; }
+        public DateTime dateTimeEnd { get; set; }
+        public Boolean isComplite { get; set; }
+    }
+}
