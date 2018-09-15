c6e15aa547c6d7fa0f1b7f2a9adb341478858228
Riskenzi <riskenzi@icloud.com>
2018-09-15 18:49:13 +0300

    task project create

--- ASP.NET_PersonControl/ASP.NET_PersonControl/Controllers/TasksController.cs
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/Controllers/TasksController.cs
@@ -162,9 +162,9 @@ namespace ASP.NET_PersonControl.Controllers
                     user = employee,
                     groups = groupList
                 };
-                return View("Create", viewModel);
+                return View("CreateTaskForGroups", viewModel);
             }
-            if (_context.TasksForUser.FirstOrDefault(c => c.Id == taskForGroupsViewModel.taskForGroups.Id) == null)
+            if (_context.TasksForGroups.FirstOrDefault(c => c.Id == taskForGroupsViewModel.taskForGroups.Id) == null)
             {
 
                 
@@ -196,9 +196,22 @@ namespace ASP.NET_PersonControl.Controllers
 
         public ActionResult CreateTaskForProjects()
         {
-            
 
-            return View();
+            _context = new ApplicationDbContext();
+
+
+            string id = User.Identity.GetUserId();
+
+            ApplicationUser employee = _context.Users.SingleOrDefault(emp => emp.Id == id);
+            List<Projects> projectList = _context.Projects.Select(g => g).ToList<Projects>();
+            var viewModel = new TaskForProjectsViewModel
+            {
+                tasksForProjects   = new TasksForProjects(),
+                user = employee,
+                projects = projectList
+            };
+
+            return View(viewModel);
 
         }
 
@@ -216,6 +229,51 @@ namespace ASP.NET_PersonControl.Controllers
             {
                 return View();
             }
+        }
+
+
+        public ActionResult SaveProjectTask(TaskForProjectsViewModel taskForProjectsViewModel)
+        {
+            _context = new ApplicationDbContext();
+            string id = User.Identity.GetUserId();
+            if (taskForProjectsViewModel.tasksForProjects.description == null || taskForProjectsViewModel.tasksForProjects.dateTimeEnd == null || taskForProjectsViewModel.tasksForProjects.dateTimeBegin == null)
+            {
+                ApplicationUser employee = _context.Users.SingleOrDefault(emp => emp.Id == id);
+                List<Groups> groupList = _context.Groups.Select(g => g).ToList<Groups>();
+                var viewModel = new TaskForGroupsViewModel
+                {
+                    taskForGroups = new TasksForGroups(),
+                    user = employee,
+                    groups = groupList
+                };
+                return View("CreateTaskForProjects", viewModel);
+            }
+            if (_context.TasksForProjects.FirstOrDefault(c => c.Id == taskForProjectsViewModel.tasksForProjects.Id) == null)
+            {
+
+
+                taskForProjectsViewModel.tasksForProjects.fromUserId = taskForProjectsViewModel.user.Id;
+                taskForProjectsViewModel.tasksForProjects.toProjectId = taskForProjectsViewModel.project.Id;
+                var result = taskForProjectsViewModel.tasksForProjects;
+                _context.TasksForProjects.Add(result);
+            }
+            else
+            {
+                //Projects projects = _context.Projects.FirstOrDefault(c => c.Id == projectController.project.Id);
+                //TasksForUser tasksForUser = _context.TasksForUser.FirstOrDefault(c=>c.Id == tasksForUser.Id)
+                //projects.Customer = projectController.customer.Id;
+                //projects.Title = projectController.project.Title;
+                //projects.Description = projectController.project.Description;
+                //projects.PriceInDollars = projectController.project.PriceInDollars;
+                //projects.isComplite = projectController.project.isComplite;
+                //projects.BeginTime = projectController.project.BeginTime;
+                //projects.UntilTime = projectController.project.UntilTime;
+            }
+
+
+            //_context.ProjectsGroups.RemoveRange(_context.ProjectsGroups.Select(ug => ug).Where(ug => ug.ProjId == projectController.project.Id).ToList());
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- ASP.NET_PersonControl/ASP.NET_PersonControl/ViewModels/TaskForProjectsViewModel.cs
+++ ASP.NET_PersonControl/ASP.NET_PersonControl/ViewModels/TaskForProjectsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP.NET_PersonControl.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,14 @@ namespace ASP.NET_PersonControl.ViewModels
 {
     public class TaskForProjectsViewModel
     {
+        //public Tasf taskForGroups { get; set; }
+
+        public TasksForProjects tasksForProjects {get; set;}
+        public ApplicationUser user { get; set; }
+
+        public Projects project { get; set; }
+
+        public IEnumerable<Projects> projects { get; set; }
+        
     }
 }
